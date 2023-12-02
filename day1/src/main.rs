71d19465cb43dbb0e9dd46c02d85c8aeed7947b1
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-12-01 18:08:20 -0800

    Solved problem 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,19 @@
+use std::fs;
+
+fn part1() {
+    let mut result = 0;
+    for line in fs::read_to_string("input.dat").unwrap().lines() {
+        let nums = line
+            .chars()
+            .filter(|c| c.is_ascii_digit())
+            .map(|c| c as u8 - b'0')
+            .collect::<Vec<_>>();
+        result +=
+            (*nums.iter().nth(0).unwrap() as i32) * 10 + (*nums.iter().last().unwrap() as i32);
+    }
+    println!("{}", result);
+}
+
 fn main() {
-    println!("Hello, world!");
+    part1();
 }
