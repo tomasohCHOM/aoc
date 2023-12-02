a6620278c55f69e6a86a8cf17bc78b82050cb2ff
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-12-01 18:49:45 -0800

    Completed day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -14,6 +14,36 @@ fn part1() {
     println!("{}", result);
 }
 
+fn part2() {
+    // Functional programming... yay
+    let result: u32 = fs::read_to_string("input.dat")
+        .unwrap()
+        .lines()
+        .filter(|line| !line.is_empty())
+        .map(|line| {
+            line.to_string()
+                .replace("one", "one1one")
+                .replace("two", "two2two")
+                .replace("three", "three3three")
+                .replace("four", "four4four")
+                .replace("five", "five5five")
+                .replace("six", "six6six")
+                .replace("seven", "seven7seven")
+                .replace("eight", "eight8eight")
+                .replace("nine", "nine9nine")
+        })
+        .map(|line| {
+            line.chars()
+                .filter_map(|c| c.to_digit(10))
+                .collect::<Vec<u32>>()
+        })
+        .map(|vec| 10 * vec.first().unwrap() + vec.last().unwrap())
+        .sum();
+
+    println!("{}", result);
+}
+
 fn main() {
     part1();
+    part2();
 }
