a4e10d8d770bf7d099c90c1b16f10f4dd0b1a910
Tomas Oh <tomasoh@csu.fullerton.edu>
2026-02-21 22:36:53 -0600

    update template.zig so we can use sample files :)

--- templates/template.zig
+++ templates/template.zig
@@ -10,9 +10,21 @@ fn part2(_: anytype) u32 {
 
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
-    const cwd = std.Io.Dir.cwd();
+    var args = init.minimal.args.iterate();
+    _ = args.skip();
 
-    var file = try cwd.openFile(io, "input.txt", .{ .mode = .read_only });
+    var use_sample = false;
+    while (args.next()) |arg| {
+        if (std.mem.eql(u8, arg, "--sample") or std.mem.eql(u8, arg, "-s")) {
+            std.debug.print("USING SAMPLE INPUT FILE\n", .{});
+            use_sample = true;
+            break;
+        }
+    }
+
+    const input_file = if (use_sample) "sample.txt" else "input.txt";
+    const cwd = std.Io.Dir.cwd();
+    var file = try cwd.openFile(io, input_file, .{ .mode = .read_only });
     defer file.close(io);
 
     var read_buf: [1024]u8 = undefined;
@@ -20,8 +32,8 @@ pub fn main(init: std.process.Init) !void {
     const reader = &file_reader.interface;
 
     // Use any parsing method here
-    while (try reader.takeDelimiter('\n')) |line| {
-        std.debug.print("{s}\n", .{line}); // Placeholder
+    while (try reader.takeDelimiter('\n')) |_| {
+        // Placeholder
     }
 
     std.debug.print("Part 1: {d}\n", .{part1(0)});
